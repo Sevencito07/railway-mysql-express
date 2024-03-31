2a45d26cb3bea09c5cdb85906e74b1af70d4cabb
Mister G <108514442+Sevencito07@users.noreply.github.com>
2024-03-31 15:08:38 -0500

    Update carsContrls.js

--- src/constrollers/carsContrls.js
+++ src/constrollers/carsContrls.js
@@ -2,7 +2,7 @@ import { createdP } from "../db.js"
 
 export const  getData= async (req, res)=>{
   try {
-    const [rows]= await createdP.query('SELECT * FROM test')
+    const [rows]= await createdP.query('SELECT * FROM cars2')
     res.send({rows})
   } catch (error) {
     return res.status(500).json({message:'sothing wrong'})
@@ -11,7 +11,7 @@ export const  getData= async (req, res)=>{
 
 export const getDatabyid = async(req, res)=>{
     try {
-      const [rows]= await createdP.query('SELECT * FROM test WHERE id=?', [req.params.id])
+      const [rows]= await createdP.query('SELECT * FROM cars2 WHERE id=?', [req.params.id])
       if(rows.length <= 0) return res.status(404).json({
         message:'404 not data'
       })
@@ -24,7 +24,7 @@ export const getDatabyid = async(req, res)=>{
  export const postData = async (req, res)=>{
     try {
       const {name, year}= req.body
-   const [rows] = await createdP.query('INSERT INTO test (name, year) VALUES (?, ?)',
+   const [rows] = await createdP.query('INSERT INTO cars2 (name, year) VALUES (?, ?)',
     [name, year])
     res.send({id:rows.insertId, name, year})
     } catch (error) {
@@ -35,7 +35,7 @@ export const getDatabyid = async(req, res)=>{
 
  export const  deleteData= async (req, res)=>{ 
    try {
-    const [rows]= await createdP.query('DELETE FROM test WHERE id =?',[req.params.id])
+    const [rows]= await createdP.query('DELETE FROM cars2 WHERE id =?',[req.params.id])
     if(rows.affectedRows <= 0)return res.status(404).json({
      message:"not file 404"
     })
@@ -50,7 +50,7 @@ export const getDatabyid = async(req, res)=>{
     try {
       const {id}= req.params;
     const {name, year}= req.body;
-    const [result]  = await createdP.query("UPDATE test SET name=?, year=? WHERE id=?",[name, year,id])
+    const [result]  = await createdP.query("UPDATE cars2 SET name=?, year=? WHERE id=?",[name, year,id])
     if (result.affectedRows === 0 ) return res.status(404).json({
         message:"not found "
     })
